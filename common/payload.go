ac63fa52fa1b69f70c207c92f2558375e1d0d9ee
Christian Saide <csaide@nsone.net>
2016-06-22 02:41:14 -0400

    Backing out test code

--- common/payload.go
+++ common/payload.go
@@ -1,14 +1,11 @@
 package common
 
-import "net"
-
 type Payload struct {
 	Raw       []byte
 	Packet    []byte
 	IpAddress []byte
 	Nonce     []byte
 	Length    int
-	Address   *net.UDPAddr
 }
 
 func NewTunPayload(raw []byte, packetLength int) *Payload {
