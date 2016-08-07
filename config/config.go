2da7b3e861f76418ce23a72ef1bd50e26a900482
Christian Saide <csaide@nsone.net>
2016-08-07 10:14:23 -0400

    Added ability to set the configuration for quantum from environment variables and updated one of the containers to utilize this new functionality.

--- config/config.go
+++ config/config.go
@@ -29,9 +29,10 @@ type Config struct {
 	ListenAddress string
 	ListenPort    int
 
-	Prefix string
+	Prefix  string
+	DataDir string
+	LogDir  string
 
-	LeaseTime       time.Duration
 	SyncInterval    time.Duration
 	RefreshInterval time.Duration
 
@@ -41,55 +42,87 @@ type Config struct {
 	TLSCA      string
 
 	Datastore   string
+	endpoints   string
 	Endpoints   []string
 	AuthEnabled bool
 	Username    string
 	Password    string
 }
 
-// New generates a new config object
-func New() *Config {
-	ifaceName := flag.String("interface-name", "quantum%d", "The name for the TUN interface that will be used for forwarding. Use %d to have the OS pick an available interface name.")
+func handleDefaultString(name, def string) string {
+	env := "QUANTUM_" + strings.ToUpper(strings.Replace(name, "-", "_", 10))
+	output := os.Getenv(env)
+	if output == "" {
+		return def
+	}
+	return output
+}
 
-	privateIP := flag.String("private-ip", "", "The private ip address of this node.")
-	publicIP := flag.String("public-ip", "", "The public ip address of this node.")
+func handleDefaultInt(name string, def int) int {
+	str := strconv.Itoa(def)
+	output, err := strconv.Atoi(handleDefaultString(name, str))
+	if err != nil {
+		panic(err)
+	}
+	return output
+}
 
-	laddr := flag.String("listen-address", "0.0.0.0", "The ip address to listen on for forwarded packets.")
-	lport := flag.Int("listen-port", 1099, "The ip port to listen on for forwarded packets.")
+func handleDefaultDuration(name string, def time.Duration) time.Duration {
+	str := def.String()
+	output, err := time.ParseDuration(handleDefaultString(name, str))
+	if err != nil {
+		panic(err)
+	}
+	return output
+}
 
-	prefix := flag.String("prefix", "quantum", "The etcd key that quantum information is stored under.")
-	dataDir := flag.String("data-dir", "/var/lib/quantum", "The data directory for quantum to use for persistent state.")
-	leaseTime := flag.Duration("lease-time", 300, "Lease time for the private ip address.")
-	syncInterval := flag.Duration("sync-interval", 30, "The backend sync interval")
-	refreshInterval := flag.Duration("refresh-interval", 60, "The backend lease refresh interval.")
+func (cfg *Config) handleCli() {
+	flag.StringVar(&cfg.InterfaceName, "interface-name", handleDefaultString("interface-name", "quantum%d"), "The name for the TUN interface that will be used for forwarding. Use %d to have the OS pick an available interface name.")
 
-	tlsCert := flag.String("tls-cert", "", "The client certificate to use for authentication with the backend datastore.")
-	tlsKey := flag.String("tls-key", "", "The client key to use for authentication with the backend datastore.")
-	tlsCA := flag.String("tls-ca-cert", "", "The CA certificate to authenticate the backend datastore.")
+	flag.StringVar(&cfg.PrivateIP, "private-ip", handleDefaultString("private-ip", ""), "The private ip address of this node.")
+	flag.StringVar(&cfg.PublicIP, "public-ip", handleDefaultString("public-ip", ""), "The public ip address of this node.")
 
-	datastore := flag.String("datastore", "etcd", "The datastore backend to use, either consul or etcd")
-	endpoints := flag.String("endpoints", "127.0.0.1:2379", "The datastore endpoints to use, in a comma separated list.")
-	username := flag.String("username", "", "The datastore username to use for authentication.")
-	password := flag.String("password", "", "The datastore password to use for authentication.")
+	flag.StringVar(&cfg.ListenAddress, "listen-address", handleDefaultString("listen-address", "0.0.0.0"), "The ip address to listen on for forwarded packets.")
+	flag.IntVar(&cfg.ListenPort, "listen-port", handleDefaultInt("listen-port", 1099), "The ip port to listen on for forwarded packets.")
+
+	flag.StringVar(&cfg.Prefix, "prefix", handleDefaultString("prefix", "quantum"), "The etcd key that quantum information is stored under.")
+	flag.StringVar(&cfg.DataDir, "data-dir", handleDefaultString("data-dir", "/var/lib/quantum"), "The data directory for quantum to use for persistent state.")
+	flag.StringVar(&cfg.LogDir, "log-dir", handleDefaultString("log-dir", ""), "The log directory to write logs to, if this is ommited logs are written to stdout/stderr.")
+
+	flag.DurationVar(&cfg.SyncInterval, "sync-interval", handleDefaultDuration("sync-interval", 30), "The backend sync interval")
+	flag.DurationVar(&cfg.RefreshInterval, "refresh-interval", handleDefaultDuration("refresh-interval", 60), "The backend lease refresh interval.")
+
+	flag.StringVar(&cfg.TLSCert, "tls-cert", handleDefaultString("tls-cert", ""), "The client certificate to use for authentication with the backend datastore.")
+	flag.StringVar(&cfg.TLSKey, "tls-key", handleDefaultString("tls-key", ""), "The client key to use for authentication with the backend datastore.")
+	flag.StringVar(&cfg.TLSCA, "tls-ca-cert", handleDefaultString("tls-ca-cert", ""), "The CA certificate to authenticate the backend datastore.")
+
+	flag.StringVar(&cfg.Datastore, "datastore", handleDefaultString("datastore", "etcd"), "The datastore backend to use, either consul or etcd")
+
+	flag.StringVar(&cfg.endpoints, "endpoints", handleDefaultString("endpoints", "127.0.0.1:2379"), "A comma delimited list of datastore endpoints to use.")
+	flag.StringVar(&cfg.Username, "username", handleDefaultString("username", ""), "The datastore username to use for authentication.")
+	flag.StringVar(&cfg.Password, "password", handleDefaultString("password", ""), "The datastore password to use for authentication.")
 
 	flag.Parse()
+}
 
-	parsedEndpoints := strings.Split(*endpoints, ",")
+func (cfg *Config) handleComputed() {
+	cfg.Endpoints = strings.Split(cfg.endpoints, ",")
+
 	pubkey, privkey := ecdh.GenerateECKeyPair()
+	cfg.PublicKey = pubkey
+	cfg.PrivateKey = privkey
 
-	tlsEnabled := false
-	if (*tlsCert != "" && *tlsKey != "") || *tlsCA != "" {
-		tlsEnabled = true
+	if (cfg.TLSCert != "" && cfg.TLSKey != "") || cfg.TLSCA != "" {
+		cfg.TLSEnabled = true
 	}
 
-	authEnabled := false
-	if *username != "" {
-		authEnabled = true
+	if cfg.Username != "" {
+		cfg.AuthEnabled = true
 	}
 
-	os.MkdirAll(*dataDir, os.ModeDir)
+	os.MkdirAll(cfg.DataDir, os.ModeDir)
 	machineID := make([]byte, 32)
-	machineIDPath := path.Join(*dataDir, "machine-id")
+	machineIDPath := path.Join(cfg.DataDir, "machine-id")
 	if _, err := os.Stat(machineIDPath); os.IsNotExist(err) {
 		rand.Read(machineID)
 		ioutil.WriteFile(machineIDPath, machineID, os.ModePerm)
@@ -97,29 +130,15 @@ func New() *Config {
 		buf, _ := ioutil.ReadFile(machineIDPath)
 		machineID = buf
 	}
+	cfg.MachineID = hex.EncodeToString(machineID)
 
-	return &Config{
-		InterfaceName:   *ifaceName,
-		MachineID:       hex.EncodeToString(machineID),
-		PrivateIP:       *privateIP,
-		PublicIP:        *publicIP,
-		PublicAddress:   *publicIP + ":" + strconv.Itoa(*lport),
-		PrivateKey:      privkey,
-		PublicKey:       pubkey,
-		ListenAddress:   *laddr,
-		ListenPort:      *lport,
-		Prefix:          *prefix,
-		LeaseTime:       *leaseTime,
-		SyncInterval:    *syncInterval,
-		RefreshInterval: *refreshInterval,
-		TLSCert:         *tlsCert,
-		TLSKey:          *tlsKey,
-		TLSCA:           *tlsCA,
-		TLSEnabled:      tlsEnabled,
-		Datastore:       *datastore,
-		Endpoints:       parsedEndpoints,
-		AuthEnabled:     authEnabled,
-		Username:        *username,
-		Password:        *password,
-	}
+	cfg.PublicAddress = cfg.PublicIP + ":" + strconv.Itoa(cfg.ListenPort)
+}
+
+// New generates a new config object
+func New() *Config {
+	cfg := &Config{}
+	cfg.handleCli()
+	cfg.handleComputed()
+	return cfg
 }
