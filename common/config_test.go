870aa91cacaee6f954819aad1decaec7e22f6b58
Christian Saide <csaide@nsone.net>
2016-08-13 14:15:20 -0400

    Added some more unit testing around config Environment variable replacement

--- common/config_test.go
+++ common/config_test.go
@@ -1,15 +1,29 @@
 package common
 
 import (
+	"os"
 	"testing"
+	"time"
 )
 
 func TestNewConfig(t *testing.T) {
+	os.Setenv("QUANTUM_INTERFACE_NAME", "different")
+	os.Setenv("QUANTUM_STATS_WINDOW", "10s")
+	os.Setenv("QUANTUM_LISTEN_PORT", "1")
 	cfg, err := NewConfig()
 	if err != nil {
 		t.Fatalf("NewConfig returned an error, %s", err)
 	}
 	if cfg == nil {
 		t.Fatal("NewConfig returned a blank config")
+	}
+	if cfg.InterfaceName != "different" {
+		t.Fatalf("NewConfig didn't pick up the environment variable replacement for InterfaceName")
+	}
+	if cfg.StatsWindow != 10*time.Second {
+		t.Fatalf("NewConfig didn't pick up the environment variable replacement for StatsWindow")
+	}
+	if cfg.ListenPort != 1 {
+		t.Fatalf("NewConfig didn't pick up the environment variable replacement for ListenPort")
 	}
 }
