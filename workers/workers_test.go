7c228fdb3660ccf453a2c5a16639ea46779b71b5
Christian Saide <csaide@quantumnet.ninja>
2016-11-18 21:29:06 -0500

    General cleanup

--- workers/workers_test.go
+++ workers/workers_test.go
@@ -26,37 +26,25 @@ var (
 	resolveOutgoingResult, sealResult, signResult *common.Payload
 
 	resolveIncomingMapping, resolveOutgoingMapping, testMapping *common.Mapping
-
-	intRemoteIP, intLocalIP uint32
 )
 
 func init() {
-	remoteIP := "10.8.0.1"
-	localIP := "10.8.0.2"
+	ip := "10.8.0.1"
 
-	store = &backend.Mock{
-		Mappings: make(map[uint32]*common.Mapping),
-	}
+	store = &backend.Mock{}
 	tun = inet.New(inet.MOCKInterface, nil)
 	sock = socket.New(socket.MOCKSocket, nil)
 
 	key := make([]byte, 32)
 	rand.Read(key)
+
 	block, _ := aes.NewCipher(key)
 	aesgcm, _ := cipher.NewGCM(block)
 
-	intRemoteIP = common.IPtoInt(remoteIP)
-	intLocalIP = common.IPtoInt(localIP)
-
 	testMapping = &common.Mapping{PublicKey: make([]byte, 32), Cipher: aesgcm}
 
-	store.Mappings[intRemoteIP] = testMapping
-	store.Mappings[intLocalIP] = testMapping
+	store.Mapping = testMapping
 
-	for i := 5; i < 10000; i++ {
-		store.Mappings[intRemoteIP+uint32(i)] = &common.Mapping{PublicKey: make([]byte, 32), Cipher: aesgcm}
-	}
-
-	incoming = NewIncoming(&common.Config{NumWorkers: 1, PrivateIP: remoteIP}, store, tun, sock)
-	outgoing = NewOutgoing(&common.Config{NumWorkers: 1, PrivateIP: localIP}, store, tun, sock)
+	incoming = NewIncoming(&common.Config{NumWorkers: 1, PrivateIP: ip}, store, tun, sock)
+	outgoing = NewOutgoing(&common.Config{NumWorkers: 1, PrivateIP: ip}, store, tun, sock)
 }
