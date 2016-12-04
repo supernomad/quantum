ed0745dd3705527aba0a4a36c76feb6d052bc9b4
Christian Saide <csaide@quantumnet.ninja>
2016-12-04 18:53:24 -0500

    Just one more test for now

--- common/common_test.go
+++ common/common_test.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"os"
 	"testing"
+	"time"
 )
 
 const (
@@ -195,6 +196,17 @@ func TestParseNetworkConfig(t *testing.T) {
 	}
 	if actual.Network != DefaultNetworkConfig.Network || actual.LeaseTime != DefaultNetworkConfig.LeaseTime {
 		t.Fatalf("ParseNetworkConfig returned the wrong value, got: %v, expected: %v", actual, DefaultNetworkConfig)
+	}
+}
+
+func TestParseNetworkConfigOnlyNetwork(t *testing.T) {
+	netCfg := &NetworkConfig{Network: "10.10.0.0/16"}
+	actual, err := ParseNetworkConfig(netCfg.Bytes())
+	if err != nil {
+		t.Fatal("ParseNetworkConfig returned an error:", err)
+	}
+	if actual.Network != netCfg.Network || actual.LeaseTime != 48*time.Hour {
+		t.Fatalf("ParseNetworkConfig returned the wrong value, got: %v, expected: %v", actual, netCfg)
 	}
 }
 
