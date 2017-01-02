083bfac7b70ccfda2337d79477f441b410293f2a
Christian Saide <csaide@quantumnet.ninja>
2017-01-01 23:06:57 -0500

    Some comment tweaks

--- workers/workers.go
+++ workers/workers.go
@@ -1,5 +1,7 @@
 // Copyright (c) 2016 Christian Saide <Supernomad>
 // Licensed under the MPL-2.0, for details see https://github.com/Supernomad/quantum/blob/master/LICENSE
 
-// Package workers contains the structs and logic to handle routing, encrypting, and analyzing traffic over the quantum network.
+/*
+Package workers contains the structs and logic to handle routing, encrypting, and analyzing traffic over the quantum network. Each worker handles one direction of network traffic, either incoming traffic from remote nodes or outgoing traffic destined for remote nodes.
+*/
 package workers
