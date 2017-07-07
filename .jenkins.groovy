8f47930b2990707cafe4fc9ff743581a24391405
Christian Saide <csaide@quantumnet.ninja>
2017-07-07 03:11:20 -0400

    Cleaned up the jenkins configuration

--- .jenkins.groovy
+++ .jenkins.groovy
@@ -14,14 +14,11 @@ node {
 
         builder = docker.build('builder', '--pull -f ./dist/Dockerfile.builder ./dist/')
         builder.inside('--net host') {
-            stage("Configure") {
+            stage("Dependencies") {
                 sh """
                     mkdir -p /opt/go/src/github.com/Supernomad
                     ln -s ${env.WORKSPACE} ${go_dir}
                 """
-            }
-
-            stage("Dependencies") {
                 sh "cd ${go_dir}; make setup_ci"
             }
 
@@ -39,17 +36,15 @@ node {
 
             stage('Results') {
                 junit allowEmptyResults: true, testResults: 'tests.xml'
-                step([$class: 'PlotBuilder', csvFileName: 'plot-56564010.csv', exclZero: false, group: 'benchmarks', keepRecords: false, logarithmic: false, numBuilds: '', style: 'line', title: 'Benchmarks', useDescr: false, xmlSeries: [[file: 'benchmarks.xml', nodeType: 'NODESET', url: '', xpath: '/Benchmarks/AllocsPerOp/*']], yaxis: 'Allocs (count)', yaxisMaximum: '', yaxisMinimum: ''])
-                step([$class: 'PlotBuilder', csvFileName: 'plot-56564010.csv', exclZero: false, group: 'benchmarks', keepRecords: false, logarithmic: false, numBuilds: '', style: 'line', title: 'Benchmarks', useDescr: false, xmlSeries: [[file: 'benchmarks.xml', nodeType: 'NODESET', url: '', xpath: '/Benchmarks/AllocsBytesPerOp/*']], yaxis: 'Allocs (B)', yaxisMaximum: '', yaxisMinimum: ''])
-                step([$class: 'PlotBuilder', csvFileName: 'plot-21467362.csv', exclZero: false, group: 'benchmarks', keepRecords: false, logarithmic: false, numBuilds: '', style: 'line', title: 'Time Per Call', useDescr: false, xmlSeries: [[file: 'benchmarks.xml', nodeType: 'NODESET', url: '', xpath: '/Benchmarks/AllocsPerOp/*']], yaxis: 'Time (ns)', yaxisMaximum: '', yaxisMinimum: ''])
+                step([$class: 'PlotBuilder', csvFileName: 'plot-56564010.csv', exclZero: false, group: 'benchmarks', keepRecords: false, logarithmic: false, numBuilds: '', style: 'line', title: 'Allocations Per Call', useDescr: false, xmlSeries: [[file: 'benchmarks.xml', nodeType: 'NODESET', url: '', xpath: '/Benchmarks/AllocsPerOp/*']], yaxis: 'Allocs (count)', yaxisMaximum: '', yaxisMinimum: ''])
+                step([$class: 'PlotBuilder', csvFileName: 'plot-56564010.csv', exclZero: false, group: 'benchmarks', keepRecords: false, logarithmic: false, numBuilds: '', style: 'line', title: 'Allocated Bytes Per Call', useDescr: false, xmlSeries: [[file: 'benchmarks.xml', nodeType: 'NODESET', url: '', xpath: '/Benchmarks/AllocsBytesPerOp/*']], yaxis: 'Allocs (B)', yaxisMaximum: '', yaxisMinimum: ''])
+                step([$class: 'PlotBuilder', csvFileName: 'plot-21467362.csv', exclZero: false, group: 'benchmarks', keepRecords: false, logarithmic: false, numBuilds: '', style: 'line', title: 'Time Per Call', useDescr: false, xmlSeries: [[file: 'benchmarks.xml', nodeType: 'NODESET', url: '', xpath: '/Benchmarks/NsPerOp/*']], yaxis: 'Time (ns)', yaxisMaximum: '', yaxisMinimum: ''])
                 step([$class: 'CoberturaPublisher', autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: 'coverage.xml', failUnhealthy: false, failUnstable: false, maxNumberOfBuilds: 0, onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false])
                 archiveArtifacts '*.xml, quantum'
             }
         }
     }
     finally {
-        stage("Cleanup") {
-            cleanWs()
-        }
+        cleanWs()
     }
 }
