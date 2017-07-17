ce89f03334b9f6e9abc1c3dc3df8233ea14373de
Christian Saide <csaide@quantumnet.ninja>
2017-07-17 17:44:45 -0400

    Fleshed out the device tests for TUN

--- device/device_test.go
+++ device/device_test.go
@@ -5,10 +5,12 @@ package device
 
 import (
 	"net"
+	"syscall"
 	"testing"
 	"time"
 
 	"github.com/Supernomad/quantum/common"
+	"golang.org/x/net/ipv4"
 )
 
 func TestMock(t *testing.T) {
@@ -66,5 +68,46 @@ func TestTUN(t *testing.T) {
 	}
 	if tun == nil {
 		t.Fatal("Failed to create TUN device: unhandled error")
+	}
+
+	if tun.Name() != "quantum0" {
+		t.Fatal("Failed to properly set the TUN device name.")
+	}
+
+	if len(tun.Queues()) != 1 {
+		t.Fatal("Failed to properly create the right number of TUN queues.")
+	}
+
+	buf := make([]byte, 1024)
+	payload, ok := tun.Read(0, buf)
+	if !ok {
+		t.Fatal("Failed to read the packet off the tun device")
+	}
+
+	iph := &ipv4.Header{
+		Version:  4,
+		Len:      20,
+		TOS:      0,
+		TotalLen: len(payload.Packet),
+		ID:       0,
+		Flags:    0,
+		FragOff:  0,
+		TTL:      128,
+		Protocol: syscall.ETH_P_IP,
+		Checksum: 0,
+		Src:      net.ParseIP("10.99.0.2"),
+		Dst:      net.ParseIP("10.99.0.1"),
+		Options:  nil,
+	}
+	iphBuf, _ := iph.Marshal()
+	copy(payload.Packet[0:20], iphBuf)
+
+	ok = tun.Write(0, payload)
+	if !ok {
+		t.Fatal("Failed to write the packet to the tun device")
+	}
+
+	if err := tun.Close(); err != nil {
+		t.Fatalf("Failed to close the TUN device: %s", err.Error())
 	}
 }
